ac87495e51293096a207568b26c717219e29653e
waadelessawy <82984314+waadelessawy@users.noreply.github.com>
2022-05-26 00:55:45 +0200

    login update

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -7,51 +7,35 @@ const bcrypt = require('bcrypt');
 
 
 module.exports.login=(request,response,next)=>{
-
+    
     let token;
-    console.log("data ")
+   
 
      if(request.body.email=="waad.elessawy@gmail.com"&&request.body.password=="123")
      {
+    
          token=jwt.sign({_id:1,
          email:request.body.email
          ,role:"admin"},
          "ColdPlayIsTheBest",
          {expiresIn:"1h"});
+   
     
          response.status(200).json({msg:"login",token,role:"admin"})
+       
      }
 
-     else{
-        Speaker.findOne({email:request.body.email})
-        .then(data=>{
+     else {
        
-         var validPassword = bcrypt.compareSync(request.body.password, data.password);
-       
-        //  if(data==null)
-        //   throw new Error("username and password are incorrect");
-
-          if(validPassword){
-
-            token=jwt.sign({_id:1,
-             email:request.body.email
-             ,role:"speaker"},
-             "ColdPlayIsTheBest",
-             {expiresIn:"1h"});
-             response.status(200).json({msg:"login",token,role:"speaker"})
-        }
-     })
-     .catch(error=>next(error))
-     }
-     
-
      Student.findOne({email:request.body.email})
      .then(data=>{
     
-      var validPassword = bcrypt.compareSync(request.body.password, data.password);
+     
     
       if(data==null)
-       throw new Error("username and password are incorrect");
+          throw new Error("username and password are incorrect");
+
+       var validPassword = bcrypt.compareSync(request.body.password, data.password);
 
        if(validPassword){
 
@@ -60,16 +44,46 @@ module.exports.login=(request,response,next)=>{
           ,role:"student"},
           "ColdPlayIsTheBest",
           {expiresIn:"1h"});
-          response.status(200).json({msg:"login",token,role:"student"})
+           response.status(200).json({msg:"login",token,role:"student"})
      }
   })
   .catch(error=>next(error))
   
 
+
   
 
 
     
+}
+
+Speaker.findOne({email:request.body.email})
+.then(data=>{
+
+
+
+ if(data==null)
+     throw new Error("username and password are incorrect");
+
+  var validPassword = bcrypt.compareSync(request.body.password, data.password);
+
+  if(validPassword){
+
+    token=jwt.sign({_id:1,
+     email:request.body.email
+     ,role:"speaker"},
+     "ColdPlayIsTheBest",
+     {expiresIn:"1h"});
+      response.status(200).json({msg:"login",token,role:"speaker"})
+}
+})
+.catch(error=>next(error))
+
+
+
+
+
+
 }
 
 
